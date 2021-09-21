7c9d863390733de9f94289f4e0b441ab66439607
FaizaNA07 <faizaabida44614@gmail.com>
2021-09-21 22:24:22 +0700

    change all

---
+++ resources/views/child.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Profil')
+@section('sidebar')
+    @parent
+    <p>Sidebar halaman Profil. <br> Nama : Faiza Nur Abida <br> Alamat :Lumajang</p>
+@endsection
+@section('content')
+    <p>sidebar berita : Indeks berita terkini dan terbaru hari ini dari peristiwa, kecelakaan, kriminal, hukum, berita unik, Politik, dan liputan khusus di Indonesia dan Internasional</p>
+@endsection
